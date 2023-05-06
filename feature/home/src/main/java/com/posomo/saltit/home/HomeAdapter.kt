bf5c1476b49ec8d93c96d6c79ea91296fe45b399
jgeun <pjk8926@gmail.com>
2023-05-06 20:04:48 +0900

    [Feat] Home UI에 Restaurant 데이터 적용 (#32)

---
+++ feature/home/src/main/java/com/posomo/saltit/home/HomeAdapter.kt
@@ -0,0 +1,37 @@
+package com.posomo.saltit.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.posomo.saltit.home.databinding.ItemSaltitPickRestaurantBinding
+import com.posomo.saltit.model.domain.RestaurantSummary
+
+class HomeAdapter : ListAdapter<RestaurantSummary, HomeAdapter.ViewHolder>(RestaurantSummaryCallback) {
+
+	class ViewHolder(private val binding: ItemSaltitPickRestaurantBinding) : RecyclerView.ViewHolder(binding.root) {
+		fun bind(item: RestaurantSummary) {
+			binding.item = item
+		}
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		val binding = ItemSaltitPickRestaurantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+		return ViewHolder(binding)
+	}
+
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+		holder.bind(currentList[position])
+	}
+
+	object RestaurantSummaryCallback : DiffUtil.ItemCallback<RestaurantSummary>() {
+		override fun areItemsTheSame(oldItem: RestaurantSummary, newItem: RestaurantSummary): Boolean {
+			return oldItem == newItem
+		}
+
+		override fun areContentsTheSame(oldItem: RestaurantSummary, newItem: RestaurantSummary): Boolean {
+			return oldItem.restaurantName == newItem.restaurantName
+		}
+	}
+}
--- feature/home/src/main/java/com/posomo/saltit/home/HomeFragment.kt
+++ feature/home/src/main/java/com/posomo/saltit/home/HomeFragment.kt
@@ -1,20 +1,27 @@
 package com.posomo.saltit.home
 
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import com.posomo.saltit.common_ui.base.BaseFragment
-import com.posomo.saltit.common_ui.recyclerview.CommonAdapter
+import com.posomo.saltit.common_ui.recyclerview.SpacingItemDecoration
 import com.posomo.saltit.common_ui.util.ActivityUtil
 import com.posomo.saltit.home.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-	private lateinit var commonAdapter: CommonAdapter
+	private val viewModel by viewModels<HomeViewModel>()
+	private val homeAdapter by lazy { HomeAdapter() }
 
 	override fun initView() {
 
 		bind {
-
+			adapter = homeAdapter
+			itemDecoration = SpacingItemDecoration(spacing = 40)
 		}
+
+		subscribeUI()
 
 		// StatusBar 색상 변경
 		(activity as ActivityUtil).changeStatusBarColor(
@@ -23,5 +30,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 				com.posomo.saltit.common_ui.R.color.saltit_blue_background1
 			)
 		)
+	}
+
+	private fun subscribeUI() {
+		viewModel.restaurantSummaries.observe(viewLifecycleOwner) {
+			homeAdapter.submitList(it) {
+				binding.homeRv.requestLayout()
+			}
+		}
 	}
 }
